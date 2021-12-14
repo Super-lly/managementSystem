87a88c9ced7910973e95174de54727665b18ab64
Super-lly <843182286@qq.com>
2021-12-14 09:16:34 +0800

    main

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { Container,Aside,Header,Main,Menu,MenuItem,MenuItemGroup,Submenu } from 'element-ui';
 
 Vue.config.productionTip = false
+
+Vue.use(Container);
+Vue.use(Aside);
+Vue.use(Header);
+Vue.use(Main);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup);
+Vue.use(Submenu);
 
 new Vue({
   router,
